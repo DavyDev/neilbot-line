07e12d528b3a79c3346f6bd7e8df67f4852ec3f8
Constantius Neil <18105757+DavyDev@users.noreply.github.com>
2020-04-17 12:31:25 +0700

    add message handler for multimedia

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 # utf-8 enconding
 from __future__ import unicode_literals
-import os, sys, logging, socket, json, tempfile, errno
+import os, sys, logging, socket, json
 from logging.handlers import SysLogHandler
 from argparse import ArgumentParser
 from modules import richmenu
@@ -38,24 +38,11 @@ nhandler = WebhookHandler(channel_secret)
 app = Flask(__name__)
 logger.info("neilbot is watching..")
 
-# create tmp dir for download content
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-def make_static_tmp_dir():
-    try:
-        os.makedirs(static_tmp_path)
-        logger.info("temp path created at: {}".format(static_tmp_path))
-    except OSError as exc:
-        logger.info('ERROR make_static_tmp_dir: {}'.format(exc))
-        if exc.errno == errno.EEXIST and os.path.isdir(static_tmp_path):
-            pass
-        else:
-            raise
-
 
 # importing words.json file
 with open('res/json/words.json') as f:
     wordsMsg = json.load(f)
-
+logger.info(wordsMsg)
 
 @app.errorhandler(Exception)
 def index(e):
@@ -89,61 +76,6 @@ def handle_text_message(event):
         event.reply_token, TextSendMessage(text=event.message.text))
     logger.info('text sent: {}'.format(event.message.text))
 
-@nhandler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
-def handle_content_message(event):
-    if isinstance(event.message, ImageMessage):
-        ext = 'jpg'
-    elif isinstance(event.message, VideoMessage):
-        ext = 'mp4'
-    elif isinstance(event.message, AudioMessage):
-        ext = 'm4a'
-    else:
-        return
-
-    message_content = line_bot_api.get_message_content(event.message.id)
-    try:
-        with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
-            for chunk in message_content.iter_content():
-                tf.write(chunk)
-            logger.info('chunk completed')
-            tempfile_path = tf.name
-            logger.info('tempfile path is {}'.format(tempfile_path))
-
-        dist_path = tempfile_path + '.' + ext
-        dist_name = os.path.basename(dist_path)
-        os.rename(tempfile_path, dist_path)
-
-
-        line_bot_api.reply_message(
-            event.reply_token, [
-                TextSendMessage(text='Save content.'),
-                TextSendMessage(text=request.host_url +
-                                os.path.join('static', 'tmp', dist_name))
-            ])
-    except Exception as e:
-        logger.info(e)
-
-
-@nhandler.add(MessageEvent, message=FileMessage)
-def handle_file_message(event):
-    message_content = line_bot_api.get_message_content(event.message.id)
-    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix='file-', delete=False) as tf:
-        for chunk in message_content.iter_content():
-            tf.write(chunk)
-        tempfile_path = tf.name
-
-    dist_path = tempfile_path + '-' + event.message.file_name
-    dist_name = os.path.basename(dist_path)
-    os.rename(tempfile_path, dist_path)
-
-    line_bot_api.reply_message(
-        event.reply_token, [
-            TextSendMessage(text='Save file.'),
-            TextSendMessage(text=request.host_url +
-                            os.path.join('static', 'tmp', dist_name))
-        ])
-
 
 if __name__ == "__main__":
-    make_static_tmp_dir()
     app.run(debug=False)
