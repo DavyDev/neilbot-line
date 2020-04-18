2f01276e0952c108239359fc1a9dcd92daf1e894
Constantius Neil <18105757+DavyDev@users.noreply.github.com>
2020-04-18 20:30:33 +0700

    fix requirements

--- app/__init__.py
+++ app/__init__.py
@@ -51,38 +51,9 @@ syslog.setFormatter(formatter)
 logger = logging.getLogger()
 logger.addHandler(syslog)
 logger.setLevel(logging.INFO)
-
 #logger.info("neilbot is watching..")
 print('neilbot initialized and now monitoring..')
 
-def add_to_mongo():
-    #Step 2: Create sample data
-    names = ['Kitchen', 'Animal', 'State', 'Tastey', 'Big', 'City',
-            'Fish', 'Pizza', 'Goat', 'Salty', 'Sandwich', 'Lazy', 'Fun']
-    company_type = ['LLC', 'Inc', 'Company', 'Corporation']
-    company_cuisine = ['Pizza', 'Bar Food', 'Fast Food',
-                    'Italian', 'Mexican', 'American', 'Sushi Bar', 'Vegetarian']
-    for x in range(1, 501):
-        business = {
-            'name': names[randint(0, (len(names)-1))] + ' ' + names[randint(0, (len(names)-1))] + ' ' + company_type[randint(0, (len(company_type)-1))],
-            'rating': randint(1, 5),
-            'cuisine': company_cuisine[randint(0, (len(company_cuisine)-1))]
-        }
-        #Step 3: Insert business object directly into MongoDB via isnert_one
-        result = mongo1.reviews.insert_one(business)
-        #Step 4: Print to the console the ObjectID of the new document
-        print('Created {0} of 500 as {1}'.format(x, result.inserted_id))
-    #Step 5: Tell us that you are done
-    print('finished creating 500 business reviews')
-
-def read_mongo(mongoid, collection):
-    if collection not in mongoid.list_collection_names():
-        print('[ERROR] collection "{}" doesn\'t exist!.'.format(collection))
-        return
-    else:
-        for x in mongoid[collection].find():
-            print(x)
-        return
 
 @app.errorhandler(Exception)
 def index(e):
